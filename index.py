84294b19e5c933b48e3038fcc137d80af3052ccb
Angel Adrian Pedroza <dmydweb@hotmail.com>
2020-08-17 07:24:30 -0500

    Add template html
    Template structure is added

--- index.py
+++ index.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 webapp = Flask(__name__)
 
 @webapp.route('/')
 def index():
-    return 'WorkManager'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     webapp.run(port=80)
